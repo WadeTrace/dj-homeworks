e2a73c5adfa3db9b2605a9c55acd5148c243cccb
Veys Gadzhiev <wadetrace@mail.ru>
2023-01-19 20:31:07 +0300

    calculate

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,22 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
+def omlet_view(request):
+    context = {'recipe': DATA['omlet'],
+               'servings': int(request.GET.get('servings', 1))}
+    return render(request, 'calculator/index.html', context)
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def pasta_view(request):
+    context = {'recipe': DATA['pasta'],
+               'servings': int(request.GET.get('servings', 1))}
+    return render(request, 'calculator/index.html', context)
+
+def buter_view(request):
+    context = {'recipe': DATA['buter'],
+               'servings': int(request.GET.get('servings', 1))}
+    return render(request, 'calculator/index.html', context)
+
+
+
+
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import omlet_view
+from calculator.views import pasta_view
+from calculator.views import buter_view
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+   path('omlet/', omlet_view, name="omlet"),
+   path('pasta/', pasta_view, name="pasta"),
+   path('buter/', buter_view, name="buter"),
 ]
