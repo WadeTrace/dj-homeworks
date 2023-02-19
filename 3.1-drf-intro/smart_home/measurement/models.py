77b4fc5ba8c5c9add3c694c165b6949c73cc6928
Veys Gadzhiev <vasya.gadzhiev.97@mail.ru>
2023-02-19 21:25:26 +0300

    add serializers

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+class Measurement(models.Model):
+    temperature = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
